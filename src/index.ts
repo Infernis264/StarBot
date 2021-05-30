24d7b0db6c25677db496f4d46d272e08bc745cdf
Infernis264 <43709147+Infernis264@users.noreply.github.com>
2021-05-30 02:15:05 -0700

    Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import * as TMI from "tmi.js";
+import CommandHandler from "./modules/CommandHandler";
+import ChatActivityLog from "./modules/ChatActivityLog";
+
+const CHANNELS = [""]; // the list of channels the bot is active in
+const PREFIX = "#";
+const log = new ChatActivityLog(CHANNELS);
+const commands = new CommandHandler(CHANNELS, log);
+
+const client = new TMI.client({
+	connection: {reconnect: true},
+	identity: {
+		username: "", // bot account name
+		password: "" // bot account password
+	},
+	channels: CHANNELS.slice()
+});
+
+
+client.on("message", async (channel: string, user: TMI.ChatUserstate, message: string) => {
+	let split = message.split(" ");
+	let command = split[0].match(new RegExp(`(?<=${PREFIX}).+`));
+	log.addUser(channel.replace(/#/g, ""), user.username);
+	if (command) {
+		if (CommandHandler.ENABLED.includes(command[0])) {
+			let message = await commands.handle(command[0], split.slice(1), user, channel.replace(/\W/g, ""));
+			if (message) {
+				client.say(channel, message.toString());
+			}
+		}
+	}
+});
+
+client.connect();
